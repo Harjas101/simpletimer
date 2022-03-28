d59c82c23cec00394982b0ea6abe493808262372
Harjas101 <94852117+Harjas101@users.noreply.github.com>
2022-03-28 14:52:14 -0400

    Add files via upload

---
+++ simpletimer.js
@@ -0,0 +1,9 @@
+const input = process.argv.slice(2)
+
+for (const time of input){
+  setTimeout(()=>{
+    console.log('time :>> ', time);
+    process.stdout.write('\x07');
+
+  }, (time * 1000))
+}
